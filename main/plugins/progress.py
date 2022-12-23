91169dba03bcfbc5b04de2fe2a9e691fa38256a1
kawukiandrew7777 <60316867+kawukiandrew7777@users.noreply.github.com>
2022-12-23 19:45:23 +0300

    Update progress.py

--- main/plugins/progress.py
+++ main/plugins/progress.py
@@ -39,7 +39,7 @@ async def progress_for_pyrogram(
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
 
-        tmp = progress + "GROSSS: {0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n".format(
+        tmp = progress + "GROSS: {0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
