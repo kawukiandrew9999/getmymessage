67f9a6eebce77c9b8a00028f742cbf7bfa79357f
kawukiandrew7777 <60316867+kawukiandrew7777@users.noreply.github.com>
2022-12-23 20:06:22 +0300

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -1,20 +1,20 @@
 #Github.com/Vasusen-code
 
 import os
-from .. import bot as Drone
+from .. import bot as katandika2022
 from telethon import events, Button
 
 from ethon.mystarts import start_srb
     
 S = '/' + 's' + 't' + 'a' + 'r' + 't'
 
-@Drone.on(events.callbackquery.CallbackQuery(data="set"))
+@katandika2022.on(events.callbackquery.CallbackQuery(data="set"))
 async def sett(event):    
-    Drone = event.client                    
+    Katandika2022 = event.client                    
     button = await event.get_message()
     msg = await button.get_reply_message() 
     await event.delete()
-    async with Drone.conversation(event.chat_id) as conv: 
+    async with katandika2022.conversation(event.chat_id) as conv: 
         xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
         x = await conv.get_reply()
         if not x.media:
@@ -32,9 +32,9 @@ async def sett(event):
         os.rename(path, f'./{event.sender_id}.jpg')
         await t.edit("Temporary thumbnail saved!")
         
-@Drone.on(events.callbackquery.CallbackQuery(data="rem"))
+@katandika2022.on(events.callbackquery.CallbackQuery(data="rem"))
 async def remt(event):  
-    Drone = event.client            
+    katandika2022 = event.client            
     await event.edit('Trying.')
     try:
         os.remove(f'{event.sender_id}.jpg')
@@ -42,7 +42,7 @@ async def remt(event):
     except Exception:
         await event.edit("No thumbnail saved.")                        
   
-@Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
+@katandika2022.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
     text = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @katandika2022"
     await start_srb(event, text)
